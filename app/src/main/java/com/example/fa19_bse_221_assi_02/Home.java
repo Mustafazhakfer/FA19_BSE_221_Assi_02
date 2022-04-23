df44f0d3359926cac02a7d17dd523e1d15ecc724
Mustafa Zhakfar <zhakfer@gmail.com>
2022-04-23 05:08:11 -0700

    Assignment 02

---
+++ app/src/main/java/com/example/fa19_bse_221_assi_02/Home.java
@@ -0,0 +1,15 @@
+package com.example.fa19_bse_221_assi_02;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+    }
+}
